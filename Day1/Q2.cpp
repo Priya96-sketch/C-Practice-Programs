9c9338de01278de57f3289d791d519ea4dca3647
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2020-10-31 23:24:10 +0530

    Create Q2.cpp

---
+++ Day1/Q2.cpp
@@ -0,0 +1,13 @@
+//Print number entered by the user. //
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<< "Enter the number USER!!"<<endl;
+    cin >>num;
+    cout << num <<" is the number"<<endl;
+    return 0;
+}
