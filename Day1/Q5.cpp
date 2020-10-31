f6445ed1a8436c4c3b836e705706f423e5851a25
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2020-10-31 23:40:04 +0530

    Create Q5.cpp

---
+++ Day1/Q5.cpp
@@ -0,0 +1,12 @@
+// Print the size of int, char, float and double. //
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "int size: " <<sizeof(int)<<endl;
+    cout << "char size: " <<sizeof(char)<<endl;
+    cout << "float size: " <<sizeof(float)<<endl;
+    cout << "double size: " <<sizeof(double)<<endl;
+    return 0;
+    
+}
