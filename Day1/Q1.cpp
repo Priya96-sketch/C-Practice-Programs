c0a6956942b4fa8fbed270462ed5f1192ad29c30
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2020-10-31 23:18:50 +0530

    Create Q1.cpp

---
+++ Day1/Q1.cpp
@@ -0,0 +1,10 @@
+// Hello World Program //
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "Hello World !! It's Priya here. Welcome to the world of C++."<<endl;
+    return 0;
+}
