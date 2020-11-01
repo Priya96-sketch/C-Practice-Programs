88db2a9f0cc51bd6b8f05a1fdeed47597090a844
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2020-11-01 08:45:41 +0530

    Create Q3.cpp

---
+++ Day2/Q3.cpp
@@ -0,0 +1,12 @@
+//Multiplication of two numbers using user input.//
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	int multiply;
+	cin>>a>>b;
+	multiply=a*b;
+	cout<<"Result= "<<multiply<<endl;
+	return 0;
+}
