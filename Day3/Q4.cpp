718268526f11dbeb4dacfd2e27bcf874dbcfef91
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2020-11-02 07:48:23 +0530

    Create Q4.cpp

---
+++ Day3/Q4.cpp
@@ -0,0 +1,14 @@
+// MULTIPLICATION TABLE //
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num,i;
+    cout<<"Enter the number: "<<endl;
+    cin>>num;
+    for(i=1 ; i<=10;i++){
+    cout<<num<<" * "<<i <<" = " <<num*i <<endl;
+    }
+    return 0;
+}
