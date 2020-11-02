118c91c63b3d0a68c2305b8f86e5f73426652fac
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2020-11-02 07:40:04 +0530

    Create Q3.cpp

---
+++ Day3/Q3.cpp
@@ -0,0 +1,27 @@
+// FACTORIAL //
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int yr;
+    int i;
+    cout<<"Enter the year: "<<endl;
+    cin>>yr;
+    if(yr%4==0)
+    {
+        if(yr%100==0)
+        {
+            if(yr%400==0)
+                cout<<"Leap Year "<<endl;
+            else
+                cout<<"Not a leap-year"<<endl;
+        }
+        else
+        cout<<"Leap-year"<<endl;
+    }
+    else
+    cout<<"Not a leap-year";
+
+    return 0;
+}
