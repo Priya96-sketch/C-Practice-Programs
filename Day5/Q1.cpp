abad2a51460875020ad89e54688e790a7d57fd90
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2020-11-04 21:59:39 +0530

    Update Q1.cpp

--- Day5/Q1.cpp
+++ Day5/Q1.cpp
@@ -1,1 +1,33 @@
+// PRIME NUMBER CHECK //
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num,i;
+    bool isPrime=true;
+    
+    cout<<"Enter the number: ";
+    cin>>num;
+    
+    //0 and 1 are not Prime Numbers//
+if(num==0 || num==1){
+    isPrime=false;
+}
+    
+    
+else{
+    for(i=2; i<=num/2 ;++i)
+    {
+        if(num%i==0){
+        isPrime=false;
+        break;
+        }
+    }
+}
+    if (isPrime)
+     cout<<num<<"is Prime no.";
+    else
+     cout<<num<<"is Not a prime no. ";
+	
+	return 0;
+}
